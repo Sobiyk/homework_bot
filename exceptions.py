edeef8a7afa3e035880f95ea56ae11075593d13d
Кирилл Собковский <kirill.sobkovskii@gmail.com>
2023-02-12 22:08:05 +0300

    added custom exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,22 +1,40 @@
 class TokenValidationError(Exception):
-    pass
+    """Ошибка при отсутствии необохдимых токенов."""
+
+    def __str__(self) -> str:
+        return 'Отсутствуют необходимые переменные окружения'
 
 
 class BadRequestError(Exception):
-    pass
+    """Сбой при запросе к эндпоинту."""
+
+    def __str__(self) -> str:
+        return 'Сбой при запросе к эндпоинту'
 
 
 class KeyAbsenceError(Exception):
-    pass
+    """Ошибка при отсутствии необохдимых ключей в ответе API."""
+
+    def __str__(self) -> str:
+        return 'Отсутствует ключ с названием работы'
 
 
 class WrongHomeworkDataTypeError(TypeError):
-    pass
+    """Неверный тип данных ключа homeworks в ответе API."""
+
+    def __str__(self) -> str:
+        return 'Отсутствует ключ с названием работы'
 
 
 class WrongResponseDataTypeError(TypeError):
-    pass
+    """Неверный тип данных ответа API."""
+
+    def __str__(self) -> str:
+        return 'Отсутствует ключ с названием работы'
 
 
 class UnknowVerdictError(KeyError):
-    pass
+    """Ошибка при получении неожиданного вердикта домашней работы."""
+
+    def __str__(self) -> str:
+        return 'Неожиданный вердикт домашней работы'
--- homework.py
+++ homework.py
@@ -1,13 +1,15 @@
-from http import HTTPStatus
+import http
+from json.decoder import JSONDecodeError
 import logging
 from logging.handlers import RotatingFileHandler
 import os
-import requests
 import sys
 import time
 
 from dotenv import load_dotenv
+import requests
 import telegram
+from telegram.error import TelegramError
 
 from exceptions import (TokenValidationError,
                         BadRequestError,
@@ -59,19 +61,16 @@ def check_tokens() -> None:
         TELEGRAM_CHAT_ID
     ]
     for token in env_vars:
-        try:
-            if token is None or token == '':
-                raise TokenValidationError
-        except TokenValidationError:
+        if not token:
             logger.critical('Обязательные переменные окружения отсутствуют')
-            sys.exit('Токен отсутствует')
+            raise TokenValidationError
 
 
 def send_message(bot, message) -> None:
     """Отправляет сообщение пользователю в чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-    except Exception as error:
+    except TelegramError as error:
         logger.error(f'Сбой при отправке сообщения в Telegram: {error}')
     else:
         logger.debug('Удачная отправка сообщения в Telegram')
@@ -87,21 +86,26 @@ def get_api_answer(timestamp: int) -> dict:
         )
     except requests.RequestException as error:
         logger.error(f'Сбой при запросе к эндпоинту: {error}')
-    if homework_statuses.status_code != HTTPStatus.OK:
+        raise BadRequestError
+    if homework_statuses.status_code != http.HTTPStatus.OK:
         logger.error('Сбой при запросе к эндпоинту')
         raise BadRequestError
-    return homework_statuses.json()
+    try:
+        return homework_statuses.json()
+    except JSONDecodeError as error:
+        logger.error(f'Некорректный формат ответа API: {error}')
+        sys.exit()
 
 
 def check_response(response: dict) -> dict:
     """Проверяет ответ API на соответствие документации."""
-    if type(response) != dict:
+    if type(response) is not dict:
         logger.error('Структура данных не соответствует ожиданиям')
         raise WrongResponseDataTypeError
-    if 'homeworks' not in response:
-        logger.error('Ключ homework отсутствует')
+    if 'homeworks' not in response or 'current_date' not in response:
+        logger.error('Необходимые ключи отсутствуют')
         raise KeyAbsenceError
-    if type(response['homeworks']) != list:
+    if type(response['homeworks']) is not list:
         logger.error('Данные под ключом homeworks пришли не в виде списка')
         raise WrongHomeworkDataTypeError
     return response
@@ -113,11 +117,13 @@ def parse_status(homework: dict) -> str:
         logger.error('Отсутствует ключ с названием работы')
         raise KeyAbsenceError
     homework_name = homework.get('homework_name')
+
     if homework.get('status') not in HOMEWORK_VERDICTS:
         logger.warning('Неожиданный статус домашней работы')
         raise UnknowVerdictError
-    else:
-        verdict = HOMEWORK_VERDICTS[homework.get('status')]
+
+    verdict = HOMEWORK_VERDICTS[homework.get('status')]
+
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -131,6 +137,9 @@ def main() -> None:
         try:
             api_answer = get_api_answer(timestamp)
             checked_api_answer = check_response(api_answer)
+
+            if not checked_api_answer['homeworks'][0]:
+                raise Exception
             status_message = parse_status(checked_api_answer['homeworks'][0])
 
             if status_message != curr_homework_status:
